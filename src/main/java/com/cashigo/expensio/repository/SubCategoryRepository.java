57bdfab2950233ed20319ccc261d9ce235236ca0
Karthik Reddy Maru <karthikreddy.maru@gmail.com>
2025-09-19 19:46:15 +0530

    add repositories

---
+++ src/main/java/com/cashigo/expensio/repository/SubCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.cashigo.expensio.repository;
+
+import com.cashigo.expensio.model.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {
+}
---
+++ src/main/java/com/cashigo/expensio/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.cashigo.expensio.repository;
+
+import com.cashigo.expensio.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
