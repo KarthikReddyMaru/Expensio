b43d24d7a2b5f13313ddf877d850c70f90e0c27b
Karthik Reddy Maru <karthikreddy.maru@gmail.com>
2025-09-19 16:33:49 +0530

    add token validation config

---
+++ src/main/java/com/cashigo/expensio/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.cashigo.expensio.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${keycloak.jwt-set-uri}")
+    private String jwtUri;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .oauth2ResourceServer(
+                        oauth -> oauth
+                                .jwt(
+                                        jwt -> jwt
+                                                .jwkSetUri(jwtUri)
+                                                .jwtAuthenticationConverter(new JwtAuthenticationConverter())
+                                )
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .oauth2Login(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+}
