2a38a1b3b51dac20a6a955924463490a0338b26f
Karthik Reddy Maru <karthikreddy.maru@gmail.com>
2025-10-08 08:06:47 +0530

    test(BudgetDefinition): add service tests for BudgetDefinitionService

---
+++ src/test/java/com/cashigo/expensio/service/BudgetDefinitionServiceTest.java
@@ -0,0 +1,181 @@
+package com.cashigo.expensio.service;
+
+import com.cashigo.expensio.common.security.UserContext;
+import com.cashigo.expensio.dto.BudgetDefinitionDto;
+import com.cashigo.expensio.dto.exception.NoBudgetDefinitionFoundException;
+import com.cashigo.expensio.dto.exception.NoCategoryFoundException;
+import com.cashigo.expensio.dto.mapper.BudgetDefinitionMapper;
+import com.cashigo.expensio.model.BudgetCycle;
+import com.cashigo.expensio.model.BudgetDefinition;
+import com.cashigo.expensio.model.Category;
+import com.cashigo.expensio.model.Transaction;
+import com.cashigo.expensio.repository.BudgetDefinitionRepository;
+import com.cashigo.expensio.repository.CategoryRepository;
+import com.cashigo.expensio.repository.TransactionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BudgetDefinitionServiceTest {
+
+    private String currentLoggedInUserId;
+
+    @Mock
+    private UserContext userContext;
+    @Mock
+    private BudgetDefinitionRepository budgetDefinitionRepository;
+    @Mock
+    private BudgetCycleService budgetCycleService;
+    @Mock
+    private BudgetDefinitionMapper budgetDefinitionMapper;
+    @Mock
+    private BudgetTrackingService budgetTrackingService;
+    @Mock
+    private CategoryRepository categoryRepository;
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @InjectMocks
+    private BudgetDefinitionService budgetDefinitionService;
+
+    @BeforeEach
+    public void init() {
+        currentLoggedInUserId = UUID.randomUUID().toString();
+        when(userContext.getUserId()).thenReturn(currentLoggedInUserId);
+    }
+
+    @Test
+    void whenSavingBudgetDefinition_thenItIsPersisted() {
+        Long categoryId = 2L;
+        BudgetDefinitionDto budgetDefinitionDto = createBudgetDefinitionDto(categoryId);
+        BudgetDefinition unsavedBudgetDefinition = createBudgetDefinition(categoryId);
+        BudgetDefinition savedBudgetDefinition = new BudgetDefinition();
+        BudgetDefinitionDto savedBudgetDefinitionDto = new BudgetDefinitionDto();
+        List<Transaction> transactions = List.of(new Transaction(), new Transaction(), new Transaction());
+        BudgetCycle budgetCycle = new BudgetCycle();
+
+        when(budgetDefinitionMapper.mapToEntity(budgetDefinitionDto)).thenReturn(unsavedBudgetDefinition);
+        when(categoryRepository.existsCategoryById(categoryId, currentLoggedInUserId)).thenReturn(true);
+        when(budgetCycleService.createBudgetCycle(unsavedBudgetDefinition)).thenReturn(budgetCycle);
+        when(budgetDefinitionRepository.save(unsavedBudgetDefinition)).thenReturn(savedBudgetDefinition);
+        when(budgetTrackingService.getTransactionsOfCurrentBudgetCycle(budgetCycle)).thenReturn(transactions);
+        when(budgetDefinitionMapper.mapToDto(any(BudgetDefinition.class))).thenReturn(savedBudgetDefinitionDto);
+
+        BudgetDefinitionDto actualDto = budgetDefinitionService.saveBudgetDefinition(budgetDefinitionDto);
+
+        InOrder inOrder = inOrder(categoryRepository, budgetCycleService,
+                budgetDefinitionRepository, budgetTrackingService, transactionRepository);
+        inOrder.verify(categoryRepository).existsCategoryById(categoryId, currentLoggedInUserId);
+        inOrder.verify(budgetCycleService).createBudgetCycle(unsavedBudgetDefinition);
+        inOrder.verify(budgetDefinitionRepository).save(unsavedBudgetDefinition);
+        inOrder.verify(budgetTrackingService).getTransactionsOfCurrentBudgetCycle(budgetCycle);
+        inOrder.verify(transactionRepository).saveAll(transactions);
+
+        assertThat(actualDto).isEqualTo(savedBudgetDefinitionDto);
+    }
+
+    @Test
+    void whenSavingBudgetDefinitionWithInvalidCategory_thenExceptionIsThrown() {
+        Long categoryId = 2L;
+        BudgetDefinitionDto budgetDefinitionDto = createBudgetDefinitionDto(categoryId);
+        BudgetDefinition unsavedBudgetDefinition = createBudgetDefinition(categoryId);
+
+        when(budgetDefinitionMapper.mapToEntity(budgetDefinitionDto)).thenReturn(unsavedBudgetDefinition);
+        when(categoryRepository.existsCategoryById(categoryId, currentLoggedInUserId)).thenReturn(false);
+
+        assertThatThrownBy(() ->
+                budgetDefinitionService.saveBudgetDefinition(budgetDefinitionDto)
+        ).isInstanceOf(NoCategoryFoundException.class);
+
+        InOrder inOrder = inOrder(categoryRepository, budgetCycleService,
+                budgetDefinitionRepository, budgetTrackingService, transactionRepository);
+        inOrder.verify(categoryRepository).existsCategoryById(categoryId, currentLoggedInUserId);
+        inOrder.verify(budgetCycleService, never()).createBudgetCycle(unsavedBudgetDefinition);
+        inOrder.verify(budgetDefinitionRepository, never()).save(unsavedBudgetDefinition);
+        inOrder.verify(budgetTrackingService, never()).getTransactionsOfCurrentBudgetCycle(any(BudgetCycle.class));
+        inOrder.verify(transactionRepository, never()).saveAll(anyList());
+    }
+
+    @Test
+    void whenUpdatingBudgetDefinition_thenOnlyAmountIsUpdated() {
+        Long categoryId = 3L;
+        BigDecimal updatedAmount = BigDecimal.valueOf(2999L);
+        UUID budgetDefinitionId = UUID.randomUUID();
+        BudgetDefinitionDto budgetDefinitionDto = createBudgetDefinitionDto(categoryId);
+        budgetDefinitionDto.setBudgetAmount(updatedAmount);
+        budgetDefinitionDto.setId(budgetDefinitionId);
+        BudgetDefinition fetchedBudgetDefinition = createBudgetDefinition(categoryId);
+        BudgetDefinition savedBudgetDefinition = new BudgetDefinition();
+        BudgetDefinitionDto savedBudgetDefinitionDto = new BudgetDefinitionDto();
+
+        when(budgetDefinitionRepository.findBudgetDefinitionByIdAndUserId(budgetDefinitionId, currentLoggedInUserId))
+                .thenReturn(Optional.of(fetchedBudgetDefinition));
+        when(budgetDefinitionRepository.save(fetchedBudgetDefinition)).thenReturn(savedBudgetDefinition);
+        when(budgetDefinitionMapper.mapToDto(savedBudgetDefinition)).thenReturn(savedBudgetDefinitionDto);
+
+        budgetDefinitionService.updateBudgetDefinition(budgetDefinitionDto);
+
+        verify(budgetDefinitionRepository).save(
+                argThat(definition ->
+                        definition.getBudgetAmount().equals(updatedAmount) &&
+                        definition.getUserId().equals(currentLoggedInUserId)
+                )
+        );
+        verify(budgetDefinitionMapper).mapToDto(savedBudgetDefinition);
+    }
+
+    @Test
+    void whenUpdatingOtherUserBudgetDefinition_thenExceptionIsThrown() {
+        Long categoryId = 3L;
+        BigDecimal updatedAmount = BigDecimal.valueOf(2999L);
+        UUID budgetDefinitionId = UUID.randomUUID();
+        BudgetDefinitionDto budgetDefinitionDto = createBudgetDefinitionDto(categoryId);
+        budgetDefinitionDto.setBudgetAmount(updatedAmount);
+        budgetDefinitionDto.setId(budgetDefinitionId);
+
+        when(budgetDefinitionRepository.findBudgetDefinitionByIdAndUserId(budgetDefinitionId, currentLoggedInUserId))
+                .thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> budgetDefinitionService.updateBudgetDefinition(budgetDefinitionDto))
+                .isInstanceOf(NoBudgetDefinitionFoundException.class);
+
+        verify(budgetDefinitionRepository, never()).save(any(BudgetDefinition.class));
+        verify(budgetDefinitionMapper, never()).mapToDto(any(BudgetDefinition.class));
+    }
+
+    private BudgetDefinition createBudgetDefinition(Long categoryId) {
+        BudgetDefinition unsavedBudgetDefinition = new BudgetDefinition();
+        Category category = createCategory(categoryId);
+        unsavedBudgetDefinition.setCategory(category);
+        return unsavedBudgetDefinition;
+    }
+
+    private BudgetDefinitionDto createBudgetDefinitionDto(Long categoryId) {
+        BudgetDefinitionDto budgetDefinitionDto = new BudgetDefinitionDto();
+        budgetDefinitionDto.setCategoryId(categoryId);
+        return budgetDefinitionDto;
+    }
+
+    private Category createCategory(Long categoryId) {
+        Category category = new Category();
+        category.setId(categoryId);
+        return category;
+    }
+
+}
